799145413b46521014815c281d65dbf30bdddfc0
Meehos <meehos90@gmail.com>
2020-03-16 09:20:26 +0300

    fix: заменил sout на log.info()

--- homework-02/src/main/java/ru/otus/spring/logging/LoggingAspect.java
+++ homework-02/src/main/java/ru/otus/spring/logging/LoggingAspect.java
@@ -1,16 +1,18 @@
 package ru.otus.spring.logging;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
 @Aspect
+@Slf4j
 public class LoggingAspect {
 
     @Before("@annotation(ru.otus.spring.logging.Logger)")
     public void logBefore(JoinPoint joinPoint) {
-        System.out.println("Прокси : " + joinPoint.getThis().getClass().getName());
-        System.out.println("Класс : " + joinPoint.getTarget().getClass().getName());
-        System.out.println("Вызов метода : " + joinPoint.getSignature().getName());
+        log.info("Прокси : " + joinPoint.getThis().getClass().getName());
+        log.info("Класс : " + joinPoint.getTarget().getClass().getName());
+        log.info("Вызов метода : " + joinPoint.getSignature().getName());
     }
 }
