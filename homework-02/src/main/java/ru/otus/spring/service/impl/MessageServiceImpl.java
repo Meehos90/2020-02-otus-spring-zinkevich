88f209d8605b44379a3679b98832f725fb0f090d
Meehos <meehos90@gmail.com>
2020-03-16 15:40:36 +0300

    fix: исправил комментарии

--- homework-02/src/main/java/ru/otus/spring/service/impl/MessageServiceImpl.java
+++ homework-02/src/main/java/ru/otus/spring/service/impl/MessageServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.otus.spring.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 import ru.otus.spring.service.MessageService;
@@ -10,9 +9,8 @@ import ru.otus.spring.service.impl.localization.LocalizationProperties;
 @Service
 @RequiredArgsConstructor
 public class MessageServiceImpl implements MessageService {
-    @Autowired
-    private MessageSource messageSource;
 
+    private final MessageSource messageSource;
     private final LocalizationProperties localProps;
 
     @Override
