09a680450dea04e549c70c6fe0de064fdec2cddc
Mikhail Zinkevich <mnzinkev@mts.ru>
2020-03-22 12:38:27 +0300

    add: добавил две ShellCommands

---
+++ homework-04/src/main/java/ru/otus/spring/service/impl/ConsoleUserService.java
@@ -0,0 +1,33 @@
+package ru.otus.spring.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.stereotype.Service;
+import ru.otus.spring.logging.Logger;
+import ru.otus.spring.service.IOService;
+import ru.otus.spring.service.MessageService;
+import ru.otus.spring.service.UserService;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ConsoleUserService implements UserService {
+    private final IOService consoleIOService;
+    private final MessageService messageService;
+
+    @Logger
+    @Override
+    public String getUserInfo() {
+        consoleIOService.showMessage(messageService.getLocaleMessage("message.greeting"));
+        String name = consoleIOService.getMessage();
+        if (name.isEmpty()) {
+            log.warn("Name is empty");
+            consoleIOService.showMessage(messageService.getLocaleMessage("message.empty.full.name"));
+            return getUserInfo();
+        }
+        log.info("User name is {}", name);
+        return name;
+    }
+}
