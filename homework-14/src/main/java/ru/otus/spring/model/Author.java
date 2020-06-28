7d8ac87440e51fdfd78ef437c461db4a3efe5f38
Mikhail Zinkevich <meehos90@gmail.com>
2020-06-28 21:30:10 +0300

    add: Сделал 14 ДЗ

---
+++ homework-14/src/main/java/ru/otus/spring/model/Author.java
@@ -0,0 +1,21 @@
+package ru.otus.spring.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "fullname", nullable = false, unique = true)
+    private String fullName;
+}
